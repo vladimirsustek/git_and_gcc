6be6ab5d2afab02225c410b9b9fc7ad4e85884ff
vlasus <vladimir.sustek@hidglobal.com>
2023-11-22 13:10:02 +0100

    Added codeblocks project

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 
 void swap_variables(int* var1, int* var2)
 {
-	backup_var1 = *var1;
+	int backup_var1 = *var1;
 	*var1 = *var2;
 	*var2 = backup_var1;
 }
 
 int factorial_recursion(int n)
 {
-	if (n = 0)
+	if (n == 0)
 	{
 		return 1;
 	}
@@ -27,15 +27,15 @@ int __cdecl main(void)
 {
 	int number_one = 1;
 	int number_two = 2;
-	
+
 	printf(message);
-	
-	printf("original %s %d %s %d\n", VAR_NAME(number_one), 
+
+	printf("original %s %d %s %d\n", VAR_NAME(number_one),
 	number_one, VAR_NAME(number_two), number_two);
-	swap_variables(number_one, number_two);
-	printf("swapped %s %d %s %d\n", VAR_NAME(number_one), 
+	swap_variables(&number_one, &number_two);
+	printf("swapped %s %d %s %d\n", VAR_NAME(number_one),
 	number_one, VAR_NAME(number_two), number_two);
-	
+
 	return 0;
 }
 
