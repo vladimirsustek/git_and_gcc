1a32971170a2829a6a3b26343a3b1b3f2819ec6b
vlasus <vladimir.sustek@hidglobal.com>
2023-11-22 11:33:43 +0100

    Introduced a new variable

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include "stdio.h"
 
-int __cdecl main(void)
-const char message[] = {"HelloWorld"};
+void swap_variables(int* var1, int* var2)
 {
+	backup_var1 = *var1;
+	*var1 = *var2;
+	*var2 = backup_var1;
+}
+
+const char message[] = {"HelloWorld"};
+
+int __cdecl main(void)
+{
+	int number_one = 1;
+	int number_two = 2;
+	
 	printf(message);	
 }
+
